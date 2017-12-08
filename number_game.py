589850e329067a63b75982627e3a4c147aea5e06
Scott Schlangen <sschlangen@gmail.com>
2017-12-07 19:12:14 -0500

    added logic to tell user if/else on the random #

--- number_game.py
+++ number_game.py
@@ -5,7 +5,15 @@ secret_num = random.randint(1, 10)
 
 while True
     # get a number guess from player
+    guess = int(input("Guess a number between 1 and 10: "))
+
     # compare guess to secret number
-    # print hit/miss
+    if guess == secret_num:
+        print("You got it! My number was {}".format(secret_num))
+        break
+    else:
+        # print hit/miss
+        print("That's not my number noob")
+
 
 
