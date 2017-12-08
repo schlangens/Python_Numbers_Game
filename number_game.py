18d644ee49fb69626a3b87c854ec8f616408f5e4
Scott Schlangen <sschlangen@gmail.com>
2017-12-07 19:34:56 -0500

    added everything to game function -> and called function

--- number_game.py
+++ number_game.py
@@ -1,19 +1,20 @@
 import random
 
-# Generate a random number between 1 and 10
-secret_num = random.randint(1, 10)
+def game():
+    # Generate a random number between 1 and 10
+    secret_num = random.randint(1, 10)
 
-while True:
-    # get a number guess from player
-    guess = int(input("Guess a number between 1 and 10: "))
+    while True:
+        # get a number guess from player
+        guess = int(input("Guess a number between 1 and 10: "))
 
-    # compare guess to secret number
-    if guess == secret_num:
-        print("You got it! My number was {}".format(secret_num))
-        break
-    else:
+        # compare guess to secret number
+        if guess == secret_num:
+            print("You got it! My number was {}".format(secret_num))
+            break
+        else:
         # print hit/miss
-        print("That's not my number noob")
+            print("That's not my number noob")
 
-
+game()
 
